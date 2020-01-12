1f4fced00e955f6dc06ea7b86dd9e0431fff07ae
FontesieYTB <57833419+FontesieYTB@users.noreply.github.com>
2020-01-12 18:12:23 +0100

    a
    a

--- magicself.js
+++ magicself.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 var prefix = ("//") //Votre prefix
-var token = ("Mzk5OTYyMzc5NTIzMzI1OTYz.XhsTBQ.I2h_3O6ZMlIQTFSgBp2l26EIVzc") //Votre token
+var token = ("") //Votre token
 
 
 
